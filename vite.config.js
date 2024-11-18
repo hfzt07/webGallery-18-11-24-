54b32a555f4ceb34856c7141709d965a5e7179a7
Hafizt Athallah <113577651+hfzt07@users.noreply.github.com>
2024-11-18 22:10:52 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist'
-  }
-})
+  base: '/webGallery-18-11-24-/', // Set this to match your GitHub repo name
+});
